7bad7f3fc1038ec05447bb410c14f1d5f9f348da
aeonsiby <aeonsiby@gmail.com>
2025-04-02 09:54:25 +1000

    Delete src/game directory

--- src/game/core/PowerUpEffect.java
+++
@@ -1,6 +0,0 @@
-package game.core;
-
-public interface PowerUpEffect {
-
-    void applyEffect(Ship playership);
-}
--- src/game/utility/Logger.java
+++
@@ -1,6 +0,0 @@
-package game.utility;
-
-public interface Logger {
-
-    void log(String text);
-}
